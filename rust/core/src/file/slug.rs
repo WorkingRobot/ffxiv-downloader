3b7ad9340b4aa6ccdb0064f853a2c505ea11cde3
Asriel Camora <asriel@camora.dev>
2025-08-12 22:47:38 -0700

    Add better errors for Slug parsing

--- rust/core/src/file/slug.rs
+++ rust/core/src/file/slug.rs
@@ -1,26 +1,46 @@
 use std::{
     fmt::{self, Display},
+    num::ParseIntError,
     str::FromStr,
 };
 
-use anyhow::{Context, Result};
-use serde::{Deserialize, Serialize};
+use anyhow::Result;
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
-#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize)]
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash)]
 #[repr(transparent)]
 pub struct Slug(u32);
 
 impl FromStr for Slug {
-    type Err = anyhow::Error;
+    type Err = ParseIntError;
 
-    fn from_str(s: &str) -> Result<Self> {
-        let slug = u32::from_str_radix(s, 16).context(format!("Invalid slug format: {s}"))?;
-        Ok(Slug(slug))
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        u32::from_str_radix(s, 16).map(Slug)
     }
 }
 
 impl Display for Slug {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{:08x}", self.0)
+    }
+}
+
+impl Serialize for Slug {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_str(&self.to_string())
+    }
+}
+
+impl<'de> Deserialize<'de> for Slug {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        String::deserialize(deserializer)?
+            .parse()
+            .map_err(serde::de::Error::custom)
     }
 }
